770bced86fecba8a07aad614a5a1efd393f8c268
Adam Oana <oana_oana172@yahoo.com>
2026-02-21 14:12:32 +0200

    Modificare produse adaugate

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ from product import Product
 
 manager = ProductManager()
 
-produs1 = Product("Laptop", 4000, 4)
-produs2 = Product("TV", 2500, 6)
-produs3 = Product("PC", 3000, 3)
+produs1 = Product("Consola", 4000, 4)
+produs2 = Product("Frigider", 2500, 6)
+produs3 = Product("Cuptor", 3000, 3)
 
 manager.adauga_produse(produs1)
 manager.adauga_produse(produs2)
 manager.adauga_produse(produs3)
 
-manager.afisare_produse()
-valoarea_totala = manager.afiseaza_val_totala()
-print(f"Valoarea totala a inventarului este {valoarea_totala} lei")
 
 manager.elimina_produs_dupa_nume("Laptop")
 manager.afisare_produse()
